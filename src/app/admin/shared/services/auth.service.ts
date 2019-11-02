3576734fe0201988e1b011cdbea948784171e5d5
Evgenii Shushkov <evgenii.web.dev@gmail.com>
2019-11-02 17:56:38 +0700

    token setup

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -10,23 +10,37 @@ export class AuthService {
     constructor(private http: HttpClient) {}
 
     get token(): string {
-        return '';
+        const expDate = new Date(localStorage.getItem('fb-token-exp'));
+        if (new Date() > expDate) {
+            this.logout();
+            return null;
+        }
+        return localStorage.getItem('fb-token');
     }
 
     login(user: User): Observable<any> {
+        user.returnSecureToken = true;
         return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
             .pipe(
                 tap(this.setToken)
             );
     }
 
-    logout() {}
+    logout() {
+        this.setToken(null);
+    }
 
     isAuthenticatited(): boolean {
         return !!this.token;
     }
 
-    private setToken(response: FirebaseAuthResponse) {
-        console.log(response);
+    private setToken(response: FirebaseAuthResponse | null) {
+        if (response) {
+            const expDate = new Date(new Date().getDate() + +response.expiresIn * 1000);
+            localStorage.setItem('fb-token', response.idToken);
+            localStorage.setItem('fb-token-exp', expDate.toString());
+        } else {
+            localStorage.clear();
+        }
     }
 }
