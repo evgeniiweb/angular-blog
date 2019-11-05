ebbe064521eaa60253a96e88e4444d2db37afa7e
Evgenii Shushkov <evgenii.web.dev@gmail.com>
2019-11-05 19:10:15 +0700

    alert plugin decor and setup

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,6 +10,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SharedModule} from '../shared/shared.module';
 import {AuthGuard} from './shared/services/auth.guard';
 import {SearchPipe} from './shared/pipes/search.pipe';
+import { AlertComponent } from './shared/components/alert/alert.component';
+import {AlertService} from './shared/services/alert.service';
 
 @NgModule({
     imports: [
@@ -38,9 +40,10 @@ import {SearchPipe} from './shared/pipes/search.pipe';
         DashboardPageComponent,
         CreatePageComponent,
         EditPageComponent,
-        SearchPipe
+        SearchPipe,
+        AlertComponent
     ],
-    providers: [AuthGuard]
+    providers: [AuthGuard, AlertService]
 })
 export class AdminModule {
 
--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostsService} from '../../shared/posts.service';
 import {Post} from '../../shared/interfaces';
 import {Subscription} from 'rxjs';
+import {AlertService} from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -14,7 +15,10 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   deleteSub: Subscription;
   search = '';
 
-  constructor(private postsService: PostsService) { }
+  constructor(
+      private postsService: PostsService,
+      private alert: AlertService
+  ) { }
 
   ngOnInit() {
     this.postsSub = this.postsService.getAll().subscribe(posts => {
@@ -35,6 +39,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   remove(id: string) {
     this.deleteSub = this.postsService.remove(id).subscribe(() => {
       this.posts = this.posts.filter(post => post.id !== id);
+      this.alert.danger('Post is deleted');
     });
   }
 }
